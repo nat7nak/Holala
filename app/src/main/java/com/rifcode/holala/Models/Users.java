3acd7e034a196c42f98049709b40e71c45803229
Natnael Belay <natnaelbelay316@gmail.com>
2024-08-12 14:39:03 +0300

    first change

---
+++ app/src/main/java/com/rifcode/holala/Models/Users.java
@@ -0,0 +1,26 @@
+package com.rifcode.holala.Models;
+
+import java.util.ArrayList;
+
+public class Users {
+
+
+    private ArrayList<Images> images;
+
+    public Users() {
+    }
+
+    public Users(ArrayList<Images> images) {
+        this.images = images;
+    }
+
+    public ArrayList<Images> getImages() {
+        return images;
+    }
+
+    public void setImages(ArrayList<Images> images) {
+        this.images = images;
+    }
+
+
+}
